26cd984c18fea466876d695f9295d7832a7b07fd
Aldinei Sampaio <aldinei.sampaio@pgmais.com.br>
2021-07-26 20:50:57 -0300

    Commit inicial

---
+++ Sample_Benchmark/DateParser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Sample_Benchmark
+{
+    public class DateParser
+    {
+        public static DateTime UsingSplit(string value)
+        {
+            var splitted = value.Split("-");
+            return new DateTime(
+                int.Parse(splitted[0]),
+                int.Parse(splitted[1]),
+                int.Parse(splitted[2])
+            );
+        }
+
+        public static DateTime UsingRegex(string value)
+        {
+            var match = Regex.Match(value, @"^(\d\d\d\d)-(\d\d)-(\d\d)$");
+            if (!match.Success)
+                throw new Exception("Data inválida");
+            return new DateTime(
+                int.Parse(match.Groups[1].Value),
+                int.Parse(match.Groups[2].Value),
+                int.Parse(match.Groups[3].Value)
+            );
+        }
+
+        private static readonly Regex regex = new Regex(@"^(\d\d\d\d)-(\d\d)-(\d\d)$", RegexOptions.Compiled);
+        public static DateTime UsingCompiledRegex(string value)
+        {
+            var match = regex.Match(value);
+            if (!match.Success)
+                throw new Exception("Data inválida");
+            return new DateTime(
+                int.Parse(match.Groups[1].Value),
+                int.Parse(match.Groups[2].Value),
+                int.Parse(match.Groups[3].Value)
+            );
+        }
+
+        public static DateTime UsingSubstring(string value)
+        {
+            return new DateTime(
+                int.Parse(value.Substring(0, 4)),
+                int.Parse(value.Substring(5, 2)),
+                int.Parse(value.Substring(8, 2))
+            );
+        }
+
+        public static DateTime UsingSpan(string value)
+        {
+            return new DateTime(
+                int.Parse(value.AsSpan(0, 4)),
+                int.Parse(value.AsSpan(5, 2)),
+                int.Parse(value.AsSpan(8, 2))
+            );
+        }
+
+        public static DateTime UsingDateTimeParse(string value)
+        {
+            return DateTime.Parse(value);
+        }
+
+        public static DateTime UsingDateTimeParseExact(string value)
+        {
+            return DateTime.ParseExact(value, "yyyy-MM-dd", null);
+        }
+
+    }
+}
---
+++ Sample_Benchmark/Program.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace Sample_Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(DateParser.UsingSplit("2021-07-26"));
+            //Console.WriteLine(DateParser.UsingRegex("2021-07-26"));
+            //Console.WriteLine(DateParser.UsingCompiledRegex("2021-07-26"));
+            //Console.WriteLine(DateParser.UsingSubstring("2021-07-26"));
+            //Console.WriteLine(DateParser.UsingSpan("2021-07-26"));
+            //Console.WriteLine(DateParser.UsingDateTimeParse("2021-07-26"));
+            //Console.WriteLine(DateParser.UsingDateTimeParseExact("2021-07-26"));
+            //Console.ReadKey();
+
+            BenchmarkRunner.Run<DateTimeParserBenchmark>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class DateTimeParserBenchmark
+    {
+        private static string textToParse = "2021-07-26";
+
+        [Benchmark] public DateTime UsingSplit() => DateParser.UsingSplit(textToParse);
+        [Benchmark] public DateTime UsingRegex() => DateParser.UsingRegex(textToParse);
+        [Benchmark] public DateTime UsingCompiledRegex() => DateParser.UsingCompiledRegex(textToParse);
+        [Benchmark] public DateTime UsingSubstring() => DateParser.UsingSubstring(textToParse);
+        [Benchmark] public DateTime UsingSpan() => DateParser.UsingSpan(textToParse);
+        [Benchmark] public DateTime UsingDateTimeParse() => DateParser.UsingDateTimeParse(textToParse);
+        [Benchmark] public DateTime UsingDateTimeParseExact() => DateParser.UsingDateTimeParse(textToParse);
+    }
+
+}
